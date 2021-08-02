fa8b60b55915bf48b85794e09281a051982cbbb5
Samuel Vicente <82454063+SamuelVicente97@users.noreply.github.com>
2021-08-02 11:21:43 -0300

    Calculo de perímetro
    Programa que calcula a área de um perímetro de quatro lados iguais.

---
+++ AreaPerimetro.py
@@ -0,0 +1,7 @@
+ladoquadrado = input("Digite o valor correspondente ao lado de um quadrado: ")
+
+area = int(ladoquadrado) ** 2
+
+perimetro = 4 * int(ladoquadrado)
+
+print("Área:",area,"- Perímetro:",perimetro)
